0db27fea7577acc05e463e939a913b4c39912a43
João Marins <jgcmarins@gmail.com>
2018-05-21 09:56:08 -0300

    feat(sync-script): fix script

--- sync.sh
+++ sync.sh
@@ -1,20 +1,23 @@
 
 # update server master
 git checkout master
-git fetch origin && git pull origin master
-git push bitbucket master
+git fetch origin
+git pull origin master
+git push bitbucket-server master
 
 # update server production
 git checkout production
 git pull origin master
-git push bitbucket/production production
+git push origin production
+git push bitbucket-server production
 
 # update front master
 git checkout master-front
 git pull origin master-front
-git push bitbucket/master-front master-front
+git push bitbucket-front master-front:master
 
 # update front production
 git checkout production-front
 git pull origin master-front
-git push bitbucket/production-front production-front
+git push origin production-front
+git push bitbucket-front production-front:production
